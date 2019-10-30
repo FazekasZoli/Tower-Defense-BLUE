032119f48d077819b727a9e044f6e35d1c10c415
Tamás VECZÁN <tamas.veczan@nng.com>
2019-10-30 15:13:45 +0100

    commit

--- Tower-Defense/main.cpp
+++ Tower-Defense/main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[]) 
+{
+	Player player;
+	CritterManger cm;
+	Map map;
 
+	Engine game(player, map, tm, cm)
 
 
 	return 0;
