89945cf23874258d32604ca5e063c02172fbdd97
sirmarcello <57135341+sirmarcello@users.noreply.github.com>
2019-10-31 15:09:12 +0100

    Player osztaly

---
+++ Tower-Defense/Player.h
@@ -0,0 +1,38 @@
+#pragma once
+
+
+class Player
+{
+private:
+
+	int life;
+	int money;
+
+
+public:
+
+	//Player konstruktora
+	Player(int _life, int _money) : life(_life), money(_money) {};
+	
+	//destruktor ha kell
+	~Player();
+
+	//GETTEREK: lekérdezést biztosítja(ezzel jelenítjük majd meg a HUD-on az életet és a pénzt, constans értékek)
+	int getLife() const;
+	int getMoney() const;
+
+	//SETTEREK: arra kell, hogy tudjuk módosítani a Life és a Money értékén.
+
+	void setLife(int _life);
+	void setMoney(int _money);
+
+	// "hozzáad / elvesz metórusok" ha kell
+	int addMoney(int _money);
+	int removeMoney(int _money);
+
+	//vásárlás metódusa
+	//void pay(int _money);
+
+
+
+};
--- Tower-Defense/main.cpp
+++ Tower-Defense/main.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
 #include "Observer.h"
+#include "Player.h"
 
 int main(int argc, char *argv[]) 
 {
+	Player* player = new Player(10, 2000);
+
+	std::cout << "Jatekos elete: " << player->getLife() << std::endl;
+	std::cout << "Jatekos penze: " << player->getMoney() << std::endl;
+
+	player->addMoney(500);
+	player->removeMoney(50);
+
+	std::cout << "Jatekos elete: " << player->getLife() << std::endl;
+	std::cout << "Jatekos penze: " << player->getMoney() << std::endl;
+
 
 
 	return 0;
