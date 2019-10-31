415504f381e389e4481fa3745ec55b76887ef48f
Tamás VECZÁN <tamas.veczan@nng.com>
2019-10-31 14:39:28 +0100

    Skeleton refactored

--- Tower-Defense/main.cpp
+++ Tower-Defense/main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
+
 #include "Observer.h"
+#include "Player.h"
+#include "View.h"
+#include "Game.h"	
 
 int main(int argc, char *argv[]) 
 {
+	View* view = new View();
+	Player* player = new Player();
+	Game* game = new Game(player, view);
 
+	game->gameLogic();
 
 	return 0;
 }
