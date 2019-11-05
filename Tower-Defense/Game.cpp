c7c0bdb356fa61ce6e3450569beb9c2f4efe314c
Tamás VECZÁN <tamas.veczan@nng.com>
2019-11-05 15:48:29 +0100

    Created CritterFinished observer, waiting for Critter class to implement Move(), and CritterManager() to implement list of critters where observer can subsrcibe

--- Tower-Defense/Game.cpp
+++ Tower-Defense/Game.cpp
@@ -5,4 +5,11 @@ void Game::gameLogic()
 	_view->displayIntro();
 	_view->displayMenu();
 	_view->displayGameOver();
+
+	// crittereket fel kell iratni az observerre a játék inicializálásakor
+}
+
+void Game::updatePlayerLife()
+{
+	_player->setLife(_player->getLife() - 1);
 }
