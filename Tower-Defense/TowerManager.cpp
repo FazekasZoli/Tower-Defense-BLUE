42ab1bfdcd1a8680d594e9e0d78021c620e4f44e
m4kor <pinter1993@gmail.com>
2019-11-13 15:05:56 +0100

    TowerManager.cpp átrendezés

--- Tower-Defense/TowerManager.cpp
+++ Tower-Defense/TowerManager.cpp
@@ -11,3 +11,24 @@ TowerManager::TowerManager()
 	std::shared_ptr<Tower>tower_ptr(new Tower(BASE, b));
 	_towerTemplates.insert(std::make_pair(BASE, tower_ptr));
 }
+void TowerManager::createTowerForGame()
+{
+	_towerForGame.emplace_back(_createTower(BASE));
+}
+
+void TowerManager::upgradeTower(std::vector<std::shared_ptr<Tower>> towers)
+{
+	for (auto tower : towers)
+	{
+		tower->upgrade(towers[0]);
+	}
+
+}
+
+int TowerManager::sellTower(std::vector<std::shared_ptr<Tower>> towers)
+{
+	for (auto tower : towers)
+	{
+		return towers[0]->getSellCost();
+	}
+}
